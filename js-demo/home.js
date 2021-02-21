1a294a82a936e7afb7877309c06c60ead48e0219
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-02-21 20:05:16 +0000

    Adding new concepts
    Adding new concepts

--- js-demo/home.js
+++ js-demo/home.js
@@ -1,10 +1,10 @@
 // Display a text
 
-let price = 49.99,
-    name = 'Hiking Boots',
-    discounted = false;
+// let price = 49.99,
+//     name = 'Hiking Boots',
+//     discounted = false;
 
-showMessage(discounted);
+// showMessage(discounted);
 
 /*      we can not start a variable with a number, the problem is with the source code.
         should start wtih _ $ letter number
@@ -18,3 +18,48 @@ showMessage(discounted);
 
         when declare a variable give a value
 */
+
+// we need set a variable to a value
+// const price = 20;
+
+// showMessage(price);
+
+// Var Keyword
+// Codes executes from top to button
+// With var we don't get error as expected.. Best practices uses key let word insted of var
+// showMessage(price);
+// var price = 25;
+
+
+// Operator type of, is simbol that operates or changes some value
+// let price = 20.99;
+// showMessage(typeof price);
+
+// let price = 12;
+// showMessage(++price);
+
+//let price = 12;
+//showMessage(price++); // it increments 1 after shows the value
+//showMessage(price);
+
+/* Precendent Operadores 
+    Multiplciation and division comes first that addition and subtraction. If you want by pass the roles just uses parenteses
+*/
+
+/* Number Precision */
+//let price = 1.1 + 1.3;
+//showMessage(price);
+
+/* Negative Numbers */
+//let amount = 0;
+//showMessage(--amount);
+
+/* Strings */
+//let message = "Hello \"World\"";
+//showMessage(message);
+
+/* Interpolation - uses $ sign*/
+let name = 'Andrea';
+let message = `Hello ${name}`;
+showMessage(message);
+
