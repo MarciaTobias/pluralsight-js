d9984e1b92c784abe21eea7738f09cfd30d404b4
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-04-20 11:53:17 +0100

    Updating new lesson

--- 06 - js-demo/home.js
+++ 06 - js-demo/home.js
@@ -1,2 +1,14 @@
+/* Conditionals Using if
+let price = 20;
 
-showMessage('help');
+if (price !== 15) {
+    showMessage('discounted');
+}
+
+//§to fixed returns a string, to fix it we need use plus sign before the paratheses
+if ( +(1.1 + 1.3).toFixed(2)  === 2.4) {
+    let message = 'Hello'
+    showMessage(message);
+}
+*/
+
