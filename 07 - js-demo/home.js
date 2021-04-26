07082092cba7c508a3033637173694c88f1cdc15
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-04-26 19:15:39 +0100

    Updating lesson 7

--- 07 - js-demo/home.js
+++ 07 - js-demo/home.js
@@ -32,8 +32,29 @@ function getSecretCode(value) {
 let secretCode = getSecretCode(2);
 showMessage( secretCode );
 //showMessage( getSecretCode(2) );
-*/
+
 
 //Function Scope, once the function completes execution, any parameters and local variable disappear 
 // That's good because we can encapsulate some code and make sure it doesn't leak out into the surrouding code
-let key = 42
+let key = 42;
+
+function getSecretCode(value) {
+
+    let keyGenerator = function() {
+        let key = 12;
+        console.log('in keyGenerator: ', key);
+        return key;
+    }
+
+    let code = value * keyGenerator();
+    console.log(' in getSecretCode: ', key);//refers to the global variable key
+    return code;
+
+}
+
+let secretCode = getSecretCode(2);
+showMessage( secretCode );
+*/
+
+// New function
+changePercentOff( 30 );
