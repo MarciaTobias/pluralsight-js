c58ac0cdb6f22d1fb3b05d74c3d1d008574b4f6d
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-04-27 18:27:07 +0100

    Updating lesson 8

--- 08 - js-demo/home.js
+++ 08 - js-demo/home.js
@@ -1,4 +1,5 @@
 
+/*
 // DOM - Document Object Model
 // Objects has a several propoerties
 // we access the properties using dot sintax, person.age (dot)
@@ -18,4 +19,48 @@ person['age'] = 44;
 
 showMessage(person.age);
 
-// Methods are functions that can be attached to an object
+
+let person = {
+    name: "John",
+    age: 32,
+    partTime: false,
+    // Methods are functions that can be attached to an object
+    showInfo: function(realAge) {
+        //showMessage('in showInfo');
+        showMessage(this.name + ' is ' + realAge); //this refers to the name of THIS object (person),
+        //to work with currently object
+
+    }
+};
+
+person.showInfo(34);
+
+// There is a big difference between passing an object to a function
+// and passing a built-in type such as a string, a number or a boolean value to a function
+let message = 'Hello';
+
+function changeMessage(message) {
+    message = 'Hi!'; // this get lost, because we are passing a string and not an object
+}
+
+// this take Hello and changes to Hi
+changeMessage(message);
+
+// this prints the hello
+showMessage(message);
+
+// When we pass objects we are able to change their properties and methods
+let person = {
+    name: "John",
+    age: 32,
+    partTime: false,
+};
+
+function incrementAge(person) {
+    person.age++;
+}
+incrementAge( person )
+
+showMessage(person.age);
+*/
+
