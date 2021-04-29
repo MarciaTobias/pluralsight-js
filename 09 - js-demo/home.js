d025d0d33e1b1468cd93e04586354a5709602141
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-04-29 12:34:09 +0100

    Updating lesson 9

--- 09 - js-demo/home.js
+++ 09 - js-demo/home.js
@@ -30,8 +30,16 @@ const values3 = ['a', 'b', 'c'];
 console.log(values3);
 console.log(typeof(values3)); // Array is not build-in data type
 console.log(Array.isArray(values3)); // Method that checks if is an array
-*/
 
 const values4 = ['a', 'b', 'c'];
 values4[0] = 'aaa'; // how to change the value in a array
 console.log(values4[0]);
+*/
+
+// Manipulating arrays using methods
+const values = ['a', 'b', 'c'];
+values.push('d', 'e', 'f'); // we can push one or more values
+const last = values.pop();
+const first = values.shift();
+values.unshift('hello', 'world');
+console.log(values, last, first);
