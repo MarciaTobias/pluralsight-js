9f1a65f1bfc8f52b90934836d8832526b574701d
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-04-29 14:00:13 +0100

    Updating lesson 9

--- 09 - js-demo/home.js
+++ 09 - js-demo/home.js
@@ -34,7 +34,6 @@ console.log(Array.isArray(values3)); // Method that checks if is an array
 const values4 = ['a', 'b', 'c'];
 values4[0] = 'aaa'; // how to change the value in a array
 console.log(values4[0]);
-*/
 
 // Manipulating arrays using methods
 const values = ['a', 'b', 'c'];
@@ -43,3 +42,26 @@ const last = values.pop();
 const first = values.shift();
 values.unshift('hello', 'world');
 console.log(values, last, first);
+
+
+// Slice creates a brand new array, an slice of the original array (used to insert or delete element somewhere within the array)
+// Arguments to pass, the beginning possition and last possition (not included in the new array). Previous array still the same
+// Slice mades a copy or the original
+const values = ['a', 'b', 'c', 'd', 'e'];
+
+// Create new variable to store new array
+const newValues = values.slice(1, 4);
+
+console.log(newValues);
+console.log(values);// original array
+*/
+
+// Splice used to insert or delete elements of an original array
+// The first aurgument is the index of element to be deleted and the second is the number of item we want delete
+const values = ['a', 'b', 'c', 'd', 'e'];
+//const newValues = values.splice(2, 1);
+values.splice(2, 0, 'hello'); //to insert a value
+
+//console.log(newValues);
+
+console.log(values);// original array
