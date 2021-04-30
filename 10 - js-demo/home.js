d0935965588aefeb8d8e8ee01a854dd3bd5fbf84
marcinha-tb <41966880+MarciaTobias@users.noreply.github.com>
2021-04-30 17:54:41 +0100

    Updating lesson 10

--- 10 - js-demo/home.js
+++ 10 - js-demo/home.js
@@ -1,5 +1,25 @@
+// Global scope and function scope
+// Scope refers to the area in which you can access a variable
+// Variables get declared in a scope
+// Global scope actually refers to utils.js, home.js is continuation of the global scope
+let productId = 12345; // declared in global scope
 
-const containers = 
-    document.getElementsByClassName('container');
-containers[0].classList.add('d-none');
-console.log(containers);
+function showProductId() {
+    console.log(productId);
+}
+
+showProductId();
+
+// Best practice, usually developers creates just 1 variable on global scope as a const
+// and include any variables that we have will become properties of that object
+const app = {
+    productId: 12345,
+    userName: 'Joe',
+    orderNumber: 789
+};
+
+function showAppName() {
+    console.log(app.userName);
+}
+
+showAppName();
